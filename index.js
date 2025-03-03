c00e895a2e4f17bd2f502634375c11c3b7de75a7
Akash MP <akashmpnlr@gmail.com>
2025-03-03 16:09:32 +0530

    3.1

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-console.log("hai");
-console.log("hai");
-console.log("hai");
+const express = require('express')
+const app = express()
 
+let phoneBook = 
+    [
+        { 
+          "id": "1",
+          "name": "Arto Hellas", 
+          "number": "040-123456"
+        },
+        { 
+          "id": "2",
+          "name": "Ada Lovelace", 
+          "number": "39-44-5323523"
+        },
+        { 
+          "id": "3",
+          "name": "Dan Abramov", 
+          "number": "12-43-234345"
+        },
+        { 
+          "id": "4",
+          "name": "Mary Poppendieck", 
+          "number": "39-23-6423122"
+        }
+    ]
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(phoneBook)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
